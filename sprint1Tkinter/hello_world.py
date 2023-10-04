e855454d72b151774e8a85b66de9c4b073764018
Narciso <ncordeiror22@fpcoruna.afundacion.org>
2023-10-04 20:47:50 +0200

    Completado ejercicio 1

---
+++ sprint1Tkinter/hello_world.py
@@ -0,0 +1,8 @@
+from tkinter import Tk
+
+#Crea una instancia de la clase Tk
+root = Tk()
+#Agrega un titulo a la ventana
+root.title("Hola mundo")
+#Ejecuta el bucle principal de la aplicacion
+root.mainloop()
