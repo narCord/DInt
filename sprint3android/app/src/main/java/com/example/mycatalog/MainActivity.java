ce0ca70f2db668941bd00190c9c7919a90381044
Narciso Cordeiro <ncordeiror22@fpcoruna.afundacion.org>
2023-11-07 10:16:32 +0100

    Ejercicio 5 añadida toolbar y navigation view. En progreso

---
+++ sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mycatalog;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class MainActivity extends AppCompatActivity {
+    private Context context = this;
+    private MaterialToolbar toolbar;
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+}
