7979b82566046eee2079a048966dd460957e1cd8
Narciso Cordeiro <ncordeiror22@fpcoruna.afundacion.org>
2023-11-08 11:02:08 +0100

    Ejercicio 5 WIP

--- sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.mycatalog;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private Context context = this;
     private MaterialToolbar toolbar;
     private DrawerLayout drawerLayout;
@@ -30,5 +35,31 @@ public class MainActivity extends AppCompatActivity {
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        getTitle();
+        return false;
+    }
+
+    private int getTitle(@NonNull MenuItem menuItem){
+        switch (menuItem.getItemId()){
+            case R.id.catalog_frag_menu:
+                Toast.makeText(context, "Catalog", Toast.LENGTH_LONG).show();
+            case R.id.detail_frag_menu:
+                Toast.makeText(context, "Detail", Toast.LENGTH_LONG).show();
+        }
+        return 1;
     }
 }
